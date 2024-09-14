9261a223cb9a3ce9f1e621adda1c3616ee7d06ea
Ajayjayendran Arulraj <ajayjayendran.ar@gmail.com>
2024-09-14 14:00:33 +0530

    Updated icons and handle clear

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,3 +1,4 @@
 import { ReactComponent as CloseIcon } from "./close-icon.svg";
+import { ReactComponent as SearchIcon } from "./search-icon.svg";
 
-export { CloseIcon };
+export { CloseIcon, SearchIcon };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CloseIcon } from "../../assets/icons";
+import { CloseIcon, SearchIcon } from "../../assets/icons";
 import styles from "./Input.module.css";
 
 interface InputProps {
@@ -11,28 +11,25 @@ export const Input = ({ onChange, placeholder = "" }: InputProps) => {
   const [value, setValue] = useState("");
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value) {
-      setValue(event.target.value);
-      onChange(event.target.value);
-    }
+    setValue(event.target.value);
+    onChange(event.target.value);
   };
 
   const onClear = () => {
-    if (value) {
-      setValue("");
-      onChange("");
-    }
+    setValue("");
+    onChange("");
   };
 
   return (
     <div className={styles.container}>
+      <SearchIcon />
       <input
         placeholder={placeholder}
         value={value}
         className={styles.defaultInput}
         onChange={handleOnChange}
       />
-      <CloseIcon className={styles.icon} onClick={onClear} />
+      {value && <CloseIcon className={styles.icon} onClick={onClear} />}
     </div>
   );
 };
